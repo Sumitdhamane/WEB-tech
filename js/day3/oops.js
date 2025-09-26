aabde7422e1311d759fda55c7ecea1fe1117764b
sumitdhamane <dhamanesumit7@email.com>
2025-09-26 12:35:03 +0530

    oops

--- js/day3/oops.js
+++ js/day3/oops.js
@@ -6,7 +6,13 @@ class Person {
   personDetails() {
     return `Name : ${this.pname} contact: ${this.pcontact} Address:${this.paddress}`;
   }
+
+  constructor(name, contact, address) {
+    this.pname = name;
+    this.pcontact = contact;
+    this.paddress = address;
+  }
 }
 
-let obj = new Person();
+let obj = new Person("Om", 2580, "dombivali");
 console.log(obj.personDetails());
