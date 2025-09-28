aee63fa9c4225fe27e320d0ad041f3ed18a26d70
sumitdhamane <dhamanesumit7@email.com>
2025-09-28 10:28:30 +0530

    repl

---
+++ Nodejs/demoRepel.js
@@ -0,0 +1,8 @@
+let fname = "sumit",
+  lname = "dhamane";
+fname + lname;
+fname + " " + lname;
+function welcome(std) {
+  return "welcome you " + std + " in ait cdac";
+}
+welcome("sumit");
---
+++ Nodejs/rpel.js
@@ -0,0 +1,12 @@
+let add = (num1, num2) => {
+  return num1 + num2;
+};
+
+// > .load rpel.js
+// let add = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+// undefined
+// > add(100,52)
+// 152
