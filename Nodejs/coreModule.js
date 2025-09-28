fccc74ea0551d83aba76f93803548db0d91d0547
sumitdhamane <dhamanesumit7@email.com>
2025-09-28 11:03:06 +0530

    PATH FUNCTIONS

--- Nodejs/coreModule.js
+++ Nodejs/coreModule.js
@@ -7,3 +7,9 @@ console.log(__filename);
 console.log(path.basename(__filename));
 
 console.log(path.basename(__dirname));
+
+console.log(path.extname(__filename));
+
+console.log(path.isAbsolute(__filename));
+console.log(path.isAbsolute("../" + __filename));
+console.log(path.join(__dirname, "myNewDIR"));
