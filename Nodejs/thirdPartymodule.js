2ac84af3f8fc77638e48feb23a9454015e2b8857
sumitdhamane <dhamanesumit7@email.com>
2025-09-28 14:42:12 +0530

    use chalk dependencies

---
+++ Nodejs/thirdPartymodule.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+console.log(chalk.bold.red("Hello world!"));
+console.log(chalk.bold.bgCyan("Hello world!"));
+console.log(chalk.underline("Hello world!"));
+console.log(chalk.bgBlueBright("Hello world!"));
