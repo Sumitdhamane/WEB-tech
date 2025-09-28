e8d0cad432c103733611be0114e7ae6ebce9b853
sumitdhamane <dhamanesumit7@email.com>
2025-09-28 14:53:13 +0530

    *

--- Nodejs/thirdPartymodule.js
+++ Nodejs/thirdPartymodule.js
@@ -4,3 +4,11 @@ console.log(chalk.bold.red("Hello world!"));
 console.log(chalk.bold.bgCyan("Hello world!"));
 console.log(chalk.underline("Hello world!"));
 console.log(chalk.bgBlueBright("Hello world!"));
+
+let freiend1 = "om",
+  freiend2 = "amaan",
+  freiend3 = "shreyas";
+
+console.log(chalk.bold.bgCyan(freiend1));
+console.log(chalk.bold.bgYellowBright(freiend2));
+console.log(chalk.bold.bgGray(freiend3));
