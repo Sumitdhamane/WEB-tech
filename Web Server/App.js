7d41b4518a9c828396412c1eec147df6e1ef5bbe
sumitdhamane <dhamanesumit7@email.com>
2025-09-29 16:57:54 +0530

    expressjs

--- Web Server/App.js
+++ Web Server/App.js
@@ -10,7 +10,24 @@ const publicPath = path.join(__dirname, "/public");
 app.use(express.static(publicPath));
 
 app.get("/", (req, res, next) => {
-  res.send("simple get request");
+  //   res.send("simple get request");
+  res.sendFile(__dirname + "/public/html/index.html");
+});
+app.get("/service", (req, res, next) => {
+  //   res.send("simple get request");
+  res.sendFile(__dirname + "/public/html/service.html");
+});
+app.get("/contact", (req, res, next) => {
+  //   res.send("simple get request");
+  res.sendFile(__dirname + "/public/html/contact.html");
+});
+app.get("/about", (req, res, next) => {
+  //   res.send("simple get request");
+  res.sendFile(__dirname + "/public/html/about.html");
+});
+app.get(/.*/, (req, res, next) => {
+  //   res.send("simple get request");
+  res.sendFile(__dirname + "/public/html/error.html");
 });
 
 app.listen(port, () => {
