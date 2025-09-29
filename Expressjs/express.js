650d3dde70e688d6d03d7e05225a843fd531c313
sumitdhamane <dhamanesumit7@email.com>
2025-09-29 13:07:48 +0530

    implement express

--- Expressjs/express.js
+++ Expressjs/express.js
@@ -5,9 +5,18 @@ const host = process.env.HOST;
 const app = express();
 
 app.get("/", (req, res, next) => {
-  res.send("Simple get request");
+  res.send("Simple get Home");
+});
+app.get("/about", (req, res, next) => {
+  res.send("Simple get about");
+});
+app.get("/contact", (req, res, next) => {
+  res.send("Simple get contact");
+});
+app.get("/service", (req, res, next) => {
+  res.send("Simple get service");
 });
 
 app.listen(port, () => {
-  console.log(`server get started on ${host}:${port}`);
+  console.log(`server get started at http://${host}:${port}`);
 });
