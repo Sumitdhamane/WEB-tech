a807b6c2cbba4fb30eaf942356e0b580ca725e8a
sumitdhamane <dhamanesumit7@email.com>
2025-09-30 14:42:07 +0530

    update

--- React/myproject/src/App.js
+++ React/myproject/src/App.js
@@ -1,18 +1,18 @@
 import "./App.css";
 // import FunctiomComp from "./components/functionComp";
-// import ClassComp from "./components/ClassComp.js";
+import ClassComp from "./components/ClassComp.js";
 // import MyComp from "./components/myComp.js";
 // import FriendComp from "./components/FriendComp.js";
-import MyFunctionComp from "./components/MyFuctionComp.js";
+// import MyFunctionComp from "./components/MyFuctionComp.js";
 
 function App() {
   return (
     <div className="App">
-      {/* <FunctiomComp fname="sumit" post="CEO" />
-      <ClassComp fname="OM" post="tech lead" />
-      <MyComp />
-      <FriendComp /> */}
-      <MyFunctionComp />
+      {/* <FunctiomComp fname="sumit" post="CEO" /> */}
+      <ClassComp fname="OM" post="Technical lead" />
+      {/* <MyComp />
+      <FriendComp />
+      <MyFunctionComp /> */}
     </div>
   );
 }
--- React/myproject/src/components/ClassComp.js
+++ React/myproject/src/components/ClassComp.js
@@ -1,13 +1,41 @@
 import React, { Component } from "react";
 
 class ClassComp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      empName: "sumit",
+      empSal: 150000,
+    };
+  }
+  changeStateData = () => {
+    this.setState({
+      empName: "sumit dhamane",
+      empSal: this.state.empSal + 10000,
+    });
+  };
+
   render() {
+    const { fname, post } = this.props;
+    const { empName, empSal } = this.state;
     return (
       <div>
         <h1>Class Component</h1>
         <p>
-          Name:{this.props.fname} and Post:{this.props.post}
+          Name:{fname} and Post:{post}
         </p>
+        <h3>state and setstate</h3>
+        <p>
+          Name:{empName} and salary:{empSal}
+        </p>
+        <button
+          type="button"
+          onClick={() => {
+            this.changeStateData();
+          }}
+        >
+          Change state data
+        </button>
       </div>
     );
   }
