9378685bc0e6318ba4c38d9cb64ef8c5127fac60
sumitdhamane <dhamanesumit7@email.com>
2025-10-01 18:17:40 +0530

    fetch data from api server

--- react_project/src/Layout/ProductDash.js
+++ react_project/src/Layout/ProductDash.js
@@ -1,9 +1,45 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 const ProductDash = () => {
+  const [product, setProduct] = useState([]);
+  const fetchData = () => {
+    axios
+      .get("http://localhost:8888/product")
+      .then((res) => {
+        setProduct(res.data);
+      })
+      .catch((error) => {});
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h2>Product Dash component</h2>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>company</th>
+            <th>quantity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {product.map((item) => (
+            <tr key={item.pid}>
+              <td>{item.pid}</td>
+              <td>{item.pname}</td>
+              <td>{item.pprice}</td>
+              <td>{item.pcompany}</td>
+              <td>{item.pquantity}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
